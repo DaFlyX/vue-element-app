e14852453c1ef015eb310c6a67f1e906064c5ef7
xu <254854036@qq.com>
2019-05-08 13:48:57 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+	<div id="app">
+		111111111111
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'app',
+		data() {
+			return{
+				activeIndex2:'1',
+			};
+		},
+		methods:{
+			handleSelect(key,keyPath){
+				console.log(key,keyPath);
+			}
+		},
+		components: {
+
+		}
+	}
+</script>
+
+<style scoped>
+	#app {
+		font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+	}
+</style>
