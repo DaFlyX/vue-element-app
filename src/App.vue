08400404aa153bea76a2374e13a6f19db90fce95
xu <254854036@qq.com>
2019-05-10 17:35:57 +0800

    搭建首页、文章页、教程、社区页完成

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,35 @@
 <template>
-	<keep-alive>
-	<router-view></router-view>
-	</keep-alive>
+	<div id="app">
+		111111111111
+	</div>
 </template>
 
+<script>
+	export default {
+		name: 'app',
+		data() {
+			return{
+				activeIndex2:'1',
+			};
+		},
+		methods:{
+			handleSelect(key,keyPath){
+				console.log(key,keyPath);
+			}
+		},
+		components: {
 
+		}
+	}
+</script>
 
+<style scoped>
+	#app {
+		font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+	}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,17 +1,14 @@
 import Vue from 'vue'
-// import ElementUI from 'element-ui'
 import App from './App.vue'
-import router from './routers'
-import store from './stores'
+import router from './router'
+import store from './store'
 import './registerServiceWorker'
 import './plugins/element.js'
 
 Vue.config.productionTip = false
-// Vue.use(ElementUI);
 
 new Vue({
   router,
   store,
-  // el :'#app',
   render: h => h(App)
 }).$mount('#app')
