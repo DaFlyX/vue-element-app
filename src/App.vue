bb6d451725898327dbf938093d67e4c70ae4abfd
xu <254854036@qq.com>
2019-05-10 18:30:22 +0800

    修改部分目录结构

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,11 @@
 <template>
-	<div id="app">
-		111111111111
-	</div>
+	<keep-alive>
+		<router-view></router-view>
+	</keep-alive>
 </template>
 
-<script>
-	export default {
-		name: 'app',
-		data() {
-			return{
-				activeIndex2:'1',
-			};
-		},
-		methods:{
-			handleSelect(key,keyPath){
-				console.log(key,keyPath);
-			}
-		},
-		components: {
 
-		}
-	}
-</script>
 
 <style scoped>
-	#app {
-		font-family: 'Avenir', Helvetica, Arial, sans-serif;
-		-webkit-font-smoothing: antialiased;
-		-moz-osx-font-smoothing: grayscale;
-		text-align: center;
-		color: #2c3e50;
-		margin-top: 60px;
-	}
+	
 </style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
+import router from './routers'
+import store from './stores'
 import './registerServiceWorker'
 import './plugins/element.js'
-
+import axios from 'axios'
 Vue.config.productionTip = false
-
+Vue.prototype.axios = axios
 new Vue({
   router,
   store,
